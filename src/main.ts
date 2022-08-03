f802ec7620932b62f8fba23d0395c30d829c3bba
chneau <charles63500@gmail.com>
2022-08-03 23:17:25 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,32 @@ import { ExtensionContext, window } from "vscode";
 const intervalIds: NodeJS.Timer[] = [];
 
 export const activate = async ({ subscriptions: sub }: ExtensionContext) => {
-  sub.push(await newSBI({ fn: async () => `$(pulse) ${(await currentLoad()).currentLoad.toFixed(2)}%`, tooltip: "Current CPU load" }));
-  (await cpuTemperature()).main && sub.push(await newSBI({ fn: async () => `$(flame) ${(await cpuTemperature()).main.toFixed(2)}C`, tooltip: "CPU temperature" }));
-  sub.push(await newSBI({ fn: async () => `$(dashboard) ${(await cpuCurrentSpeed()).avg.toFixed(2)}GHz`, tooltip: "CPU average speed" }));
-  (await battery()).hasBattery && sub.push(await newSBI({ fn: async () => `$(plug) ${(await battery()).percent}%`, tooltip: "Battery charge" }));
-  sub.push(await newSBI({ fn: async () => `$(server) ${formatBytes((await mem()).active)}`, tooltip: "Memory usage" }));
-  sub.push(await newSBI({ fn: async () => `$(arrow-small-down) ${formatBytes((await networkStats())[0]?.rx_sec ?? 0)}`, tooltip: "Network download" }));
-  sub.push(await newSBI({ fn: async () => `$(arrow-small-up) ${formatBytes((await networkStats())[0]?.tx_sec ?? 0)}`, tooltip: "Network upload" }));
-  sub.push(await newSBI({ fn: async () => `$(database) ${formatBytes((await fsSize()).reduce((acc, cur) => acc + cur.used, 0))}`, tooltip: "Filesystem usage" }));
+  sub.push(await newSBI({ fn: currentLoad, text: (x) => `$(pulse)${x.currentLoad.toFixed(2)}%`, name: "CPU load" }));
+  sub.push(await newSBI({ fn: cpuTemperature, text: (x) => `$(flame)${x.main?.toFixed(2) ?? -1}C`, name: "CPU temperature" }));
+  sub.push(await newSBI({ fn: cpuCurrentSpeed, text: (x) => `$(dashboard)${x.avg.toFixed(2)}GHz`, name: "CPU speed" }));
+  sub.push(await newSBI({ fn: battery, text: (x) => `$(plug)${x.percent ?? -1}%`, name: "Battery charge" }));
+  sub.push(await newSBI({ fn: mem, text: (x) => `$(server)${formatBytes(x.active)}`, name: "Memory usage" }));
+  sub.push(await newSBI({ fn: networkStats, text: (x) => `$(arrow-small-down)${formatBytes(x[0]?.rx_sec ?? 0)}$(arrow-small-up)${formatBytes(x[0]?.tx_sec ?? 0)}`, name: "Network usage" }));
+  sub.push(await newSBI({ fn: fsSize, text: (x) => `$(database)${formatBytes(x.reduce((acc, cur) => acc + cur.used, 0))}`, name: "Filesystem usage" }));
 };
 
 export const deactivate = () => intervalIds.forEach((id) => clearInterval(id));
 
-const newSBI = async ({ fn, tooltip }: { fn: () => Promise<string>; tooltip: string }) => {
-  const sbi = window.createStatusBarItem();
-  sbi.text = await fn();
-  sbi.tooltip = tooltip;
+interface NewSBIProps<T> {
+  fn: () => Promise<T>;
+  text: (value: T) => string;
+  name: string;
+  ms?: number;
+}
+
+const newSBI = async <T>({ fn, text, name, ms = 1000 }: NewSBIProps<T>) => {
+  const sbi = window.createStatusBarItem(name);
   sbi.show();
-  const intervalId = setInterval(async () => (sbi.text = await fn()), 1000);
-  intervalIds.push(intervalId);
+  sbi.tooltip = name;
+  sbi.name = `ResMon: ${name}`;
+  const updateTexts = async () => (sbi.text = text(await fn()));
+  await updateTexts();
+  intervalIds.push(setInterval(updateTexts, ms));
   return sbi;
 };
 
