3be7881e18f3fc2ab0b699cd679cc7e8947aa6dd
chneau <charles63500@gmail.com>
2022-08-03 23:30:30 +0100

    priority

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { battery, cpuCurrentSpeed, cpuTemperature, currentLoad, fsSize, mem, networkStats } from "systeminformation";
-import { ExtensionContext, window } from "vscode";
+import { ExtensionContext, StatusBarAlignment, window } from "vscode";
 
 const intervalIds: NodeJS.Timer[] = [];
 
 export const activate = async ({ subscriptions: sub }: ExtensionContext) => {
-  sub.push(await newSBI({ fn: currentLoad, text: (x) => `$(pulse)${x.currentLoad.toFixed(2)}%`, name: "CPU load" }));
-  sub.push(await newSBI({ fn: cpuTemperature, text: (x) => `$(flame)${x.main?.toFixed(2) ?? -1}C`, name: "CPU temperature" }));
-  sub.push(await newSBI({ fn: cpuCurrentSpeed, text: (x) => `$(dashboard)${x.avg.toFixed(2)}GHz`, name: "CPU speed" }));
-  sub.push(await newSBI({ fn: battery, text: (x) => `$(plug)${x.percent ?? -1}%`, name: "Battery charge" }));
-  sub.push(await newSBI({ fn: mem, text: (x) => `$(server)${formatBytes(x.active)}`, name: "Memory usage" }));
-  sub.push(await newSBI({ fn: networkStats, text: (x) => `$(arrow-small-down)${formatBytes(x[0]?.rx_sec ?? 0)}$(arrow-small-up)${formatBytes(x[0]?.tx_sec ?? 0)}`, name: "Network usage" }));
-  sub.push(await newSBI({ fn: fsSize, text: (x) => `$(database)${formatBytes(x.reduce((acc, cur) => acc + cur.used, 0))}`, name: "Filesystem usage" }));
+  let priority = 10;
+  sub.push(newSBI({ fn: currentLoad, text: (x) => `$(pulse)${x.currentLoad.toFixed(2)}%`, name: "CPU load", priority: priority-- }));
+  sub.push(newSBI({ fn: cpuTemperature, text: (x) => `$(flame)${x.main?.toFixed(2) ?? -1}C`, name: "CPU temperature", priority: priority-- }));
+  sub.push(newSBI({ fn: cpuCurrentSpeed, text: (x) => `$(dashboard)${x.avg.toFixed(2)}GHz`, name: "CPU speed", priority: priority-- }));
+  sub.push(newSBI({ fn: battery, text: (x) => `$(plug)${x.percent ?? -1}%`, name: "Battery charge", priority: priority-- }));
+  sub.push(newSBI({ fn: mem, text: (x) => `$(server)${formatBytes(x.active)}`, name: "Memory usage", priority: priority-- }));
+  sub.push(newSBI({ fn: networkStats, text: (x) => `$(arrow-small-down)${formatBytes(x[0]?.rx_sec ?? 0)}$(arrow-small-up)${formatBytes(x[0]?.tx_sec ?? 0)}`, name: "Network usage", priority: priority-- }));
+  sub.push(newSBI({ fn: fsSize, text: (x) => `$(database)${formatBytes(x.reduce((acc, cur) => acc + cur.used, 0))}`, name: "Filesystem usage", priority: priority }));
 };
 
 export const deactivate = () => intervalIds.forEach((id) => clearInterval(id));
@@ -20,15 +21,16 @@ interface NewSBIProps<T> {
   text: (value: T) => string;
   name: string;
   ms?: number;
+  priority?: number;
 }
 
-const newSBI = async <T>({ fn, text, name, ms = 1000 }: NewSBIProps<T>) => {
-  const sbi = window.createStatusBarItem(name);
+const newSBI = <T>({ fn, text, name, ms = 1000, priority }: NewSBIProps<T>) => {
+  const sbi = window.createStatusBarItem(`ResMon: ${name}`, StatusBarAlignment.Left, priority);
   sbi.show();
   sbi.tooltip = name;
-  sbi.name = `ResMon: ${name}`;
+  sbi.name = sbi.id;
   const updateTexts = async () => (sbi.text = text(await fn()));
-  await updateTexts();
+  updateTexts();
   intervalIds.push(setInterval(updateTexts, ms));
   return sbi;
 };
