3ba97871782824c4488f5f71207fc5156d9c14c4
chneau <charles63500@gmail.com>
2022-08-03 19:45:29 +0100

    remove all rename things ok

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { commands, ExtensionContext, window } from "vscode";
 export const activate = (context: ExtensionContext) => {
   console.log('Congratulations, your extension "resource-monitor" is now active!');
 
+  const statusBarItem = window.createStatusBarItem();
+  statusBarItem.text = "Hello!";
+  statusBarItem.color = "yellow";
+  statusBarItem.show();
+
   const disposable = commands.registerCommand("resource-monitor.helloWorld", () => {
-    window.showInformationMessage("Yo!");
+    statusBarItem.color = statusBarItem.color === "yellow" ? "green" : "yellow";
   });
 
   context.subscriptions.push(disposable);
+  context.subscriptions.push(statusBarItem);
 };
 
 export const deactivate = () => ({});
