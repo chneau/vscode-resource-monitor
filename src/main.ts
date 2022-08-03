8618178ee1d6f0f6f60b0ca08accd207cea05030
chneau <charles63500@gmail.com>
2022-08-03 21:11:38 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,21 @@ import { ExtensionContext, window } from "vscode";
 const intervalIds: NodeJS.Timer[] = [];
 
 export const activate = async ({ subscriptions: sub }: ExtensionContext) => {
-  sub.push(await newSBI(async () => `$(pulse) ${(await currentLoad()).currentLoad.toFixed(2)}`));
-  (await cpuTemperature()).main && sub.push(await newSBI(async () => `$(flame) ${(await cpuTemperature()).main.toFixed(2)}%`));
-  sub.push(await newSBI(async () => `$(dashboard) ${(await cpuCurrentSpeed()).avg.toFixed(2)}GHz`));
-  (await battery()).hasBattery && sub.push(await newSBI(async () => `$(plug) ${(await battery()).percent}%`));
-  sub.push(await newSBI(async () => `$(server) ${formatBytes((await mem()).active)}`));
-  sub.push(await newSBI(async () => `$(arrow-small-down) ${formatBytes((await networkStats())[0]?.rx_sec ?? 0)}`));
-  sub.push(await newSBI(async () => `$(arrow-small-up) ${formatBytes((await networkStats())[0]?.tx_sec ?? 0)}`));
+  sub.push(await newSBI({ fn: async () => `$(pulse) ${(await currentLoad()).currentLoad.toFixed(2)}%`, tooltip: "Current CPU load" }));
+  (await cpuTemperature()).main && sub.push(await newSBI({ fn: async () => `$(flame) ${(await cpuTemperature()).main.toFixed(2)}C`, tooltip: "CPU temperature" }));
+  sub.push(await newSBI({ fn: async () => `$(dashboard) ${(await cpuCurrentSpeed()).avg.toFixed(2)}GHz`, tooltip: "CPU average speed" }));
+  (await battery()).hasBattery && sub.push(await newSBI({ fn: async () => `$(plug) ${(await battery()).percent}%`, tooltip: "Battery charge" }));
+  sub.push(await newSBI({ fn: async () => `$(server) ${formatBytes((await mem()).active)}`, tooltip: "Memory usage" }));
+  sub.push(await newSBI({ fn: async () => `$(arrow-small-down) ${formatBytes((await networkStats())[0]?.rx_sec ?? 0)}`, tooltip: "Network download" }));
+  sub.push(await newSBI({ fn: async () => `$(arrow-small-up) ${formatBytes((await networkStats())[0]?.tx_sec ?? 0)}`, tooltip: "Network upload" }));
 };
 
 export const deactivate = () => intervalIds.forEach((id) => clearInterval(id));
 
-const newSBI = async (fn: () => Promise<string>) => {
+const newSBI = async ({ fn, tooltip }: { fn: () => Promise<string>; tooltip: string }) => {
   const sbi = window.createStatusBarItem();
   sbi.text = await fn();
+  sbi.tooltip = tooltip;
   sbi.show();
   const intervalId = setInterval(async () => (sbi.text = await fn()), 1000);
   intervalIds.push(intervalId);
